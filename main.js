ebedf6efb838944447d31039e6d7447ba346db75
theyashcode <112233yashraj@gmail.com>
2026-08-12 21:22:47 +0530

    Add files via upload
    just change

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-
 $(function(){
 
-    $("#portfolio .btn").click( function (){   
+     $("body").addClass("intro-active");
+
+$("#crack-intro").click(function(){
+    $(this).addClass("cracked");
+    $("body").removeClass("intro-active");
+    setTimeout(function(){
+        $("#crack-intro").remove();
+    }, 900);
+});
+
+    $("#portfolio .btn").click( function (){
 
         $("#portfolio .btn").removeClass("fill");
         $(this).addClass("fill");
@@ -23,39 +32,39 @@ $(function(){
 
 
     })
-    
-  
+
+
     $(".more").click(function (){
         $(this).children("span").toggleClass('fa-minus-circle')
-       $("#portfolio .all:nth-child(7),#portfolio .all:nth-child(8),#portfolio .all:nth-child(9)").slideToggle()                               
-    })               
+       $("#portfolio .all:nth-child(7),#portfolio .all:nth-child(8),#portfolio .all:nth-child(9)").slideToggle()
+    })
 
    $(".seemore").click(function (){
     $(this).children("span").toggleClass('fa-minus-circle')
     $("#about .all:nth-child(4),#about .all:nth-child(5),#about .all:nth-child(6)").slideToggle()
    })
-    
+
    $(".accordion a").click(function(e){
     e.preventDefault();
-    
-     if(!$(this).hasClass(".active")){
-      $(".accordion a").removeClass(".active")
-      $(this).addClass(".active")
+
+     if(!$(this).hasClass("active")){
+      $(".accordion a").removeClass("active")
+      $(this).addClass("active")
       $(".accordion p").slideUp();
       $(this).next("p").slideDown();
     }
    })
 
    $(".wrap-13 button").click(function () {
-    $(window).scrollTop(0)
+    $("html, body").animate({ scrollTop: 0 }, 400);
    })
 
   $(".signUp").click(function(){
-    $("body").append(`<div class = "overlay"></div>`)
-    $("body").append(`<div class = "popover"></div>`)
+    $("body").append(`<div class="overlay"></div>`)
+    $("body").append(`<div class="popover"></div>`)
     $("body").css({"overflow" : "hidden"})
 
-   
+
   $(".popover").load("form.html",function (){
     $(".close").click(function(){
      $(".popover").remove();
@@ -63,17 +72,17 @@ $(function(){
      $("body").css({"overflow" : "visible"})
     })
      })
-  
+
     $(".overlay").click(function (){
       $(".popover").remove();
       $(".overlay").remove();
       $("body").css({"overflow" : "visible"})
     })
 
-    $(window).on("keydown", function (){
+    $(document).on("keydown", function (e){
       if(e.which==27){
-        $("popover").remove();
-        $("overlay").remove();
+        $(".popover").remove();
+        $(".overlay").remove();
         $("body").css({"overflow" : "visible"})
       }
     })
@@ -88,7 +97,8 @@ $(function(){
 
 if(screen.width<768){
     $("nav a").click(function(){
-        $(".collapse ").slideUp();
+        $(".collapse").slideUp();
+        $(".menu").removeClass("active");
     })
 }
 
@@ -96,22 +106,19 @@ $(".banner a , .banner button").click(function (e){
   e.preventDefault();
   $(".banner video").fadeIn();
   $(".banner .flex").fadeOut(0);
-  $(".banner").css({"padding" : " 20px 0"})
+  $(".banner").css({"padding" : "20px 0"})
   $(".banner video").attr("src", "techaltum.webm")
-  $(".banner video").trigger("play");
+  $(".banner video")[0].play();
 })
 
-$(".reviews").bxSlider(
-  {
-    controls: false,
-    auto : true ,
-    autoHover: true,
-
-  }
-)
-
-
+if ($.fn.bxSlider) {
+  $(".reviews").bxSlider(
+    {
+      controls: false,
+      auto : true ,
+      autoHover: true,
+    }
+  )
+}
 
 })
-
-
